49e8bd09705076332671a6731ce3f1db58957489
heejin <31716791+h2jin1@users.noreply.github.com>
2019-06-25 20:03:18 +0900

    create app

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <h1>{{msgValue}}</h1>
+    <button @click="showLog">Show</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  // props: {
+  //   msg: String
+  // }
+  // props - 상위(부모)에서 하위(자식)로 key 값을 넘길 때 
+  props: ['msgValue'],
+  methods: {
+    showLog: function(e) {
+      // 이벤트가 발생했다는 것을 상위 컴포넌트로 보냄.
+      this.$emit('show-log');
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <input type="text" v-model="newTodoItem">
+        <button v-on:click="addTodo">추가</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            newTodoItem: ''
+        }
+    },
+    methods: {
+        addTodo: function() {
+            
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
