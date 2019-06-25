634f61e853b6c4e4232a9cd17f2bcb5f121f1ef5
heejin <31716791+h2jin1@users.noreply.github.com>
2019-06-25 20:58:33 +0900

    todo Input and List add

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,25 +1,77 @@
 <template>
-    <div>
-        <input type="text" v-model="newTodoItem">
-        <button v-on:click="addTodo">추가</button>
-    </div>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" @keyup.enter="addTodo">
+    <!-- <button v-on:click="addTodo">추가</button> -->
+    <span class="addContainer" @click="addTodo">
+      <i class="fas fa-plus addBtn"></i>
+    </span>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            newTodoItem: ''
-        }
+  data() {
+    return {
+      newTodoItem: ""  //변수 초기화
+    };
+  },
+  methods: {
+    // addTodo: function() {
+    //   if (this.newTodoItem !== "") {  //validation 체크
+    //     localStorage.setItem(this.newTodoItem, this.newTodoItem);
+    //     this.clearInput();
+    //   }
+    // },
+    addTodo: function() {
+        if(this.newTodoItem !== "") {
+            var obj = {completed: false, item: this.newTodoItem};
+            localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
+            this.clearInput();
+        }    
     },
-    methods: {
-        addTodo: function() {
-            
-        }
-    },
-}
+    clearInput: function() {
+      this.newTodoItem = "";
+    }
+  }
+};
 </script>
 
 <style>
-
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,15 +1,70 @@
 <template>
-    <div>
-        <h3>List</h3>
-    </div>
+  <div>
+    <ul>
+      <li v-for="(todoItem, index) in todoItems" :key="index" class="shadow">
+        {{todoItem}}
+        <span class="removeBtn" @click="removeTodo(todoItem, index)">
+          <i class="fas fa-trash-alt"></i>
+        </span>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      todoItems: [] //배열 초기화
+    };
+  },
+  created() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  },
+  methods: {
+    removeTodo: function(todoItem, index) {
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    }
+  }
+};
 </script>
 
-<style>
-
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
 </style>
